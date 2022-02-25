b142d73dde6907dd7e127afec2862d689d832094
Paqimm <mcgeewilliam815@gmail.com>
2022-02-25 11:42:32 +0300

    chore (Endpoints) : users endpoints endpoints for adding a user and getting all users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin,AbstractUser
 
-# Create your models here.
+class User(AbstractUser,PermissionsMixin):
+    faculty= models.CharField(max_length=255)
+    course= models.CharField(max_length=255)
+    registration_number= models.CharField(max_length=8,unique=True)
+    campus= models.CharField(max_length=255)
+    mode_of_study= models.CharField(max_length=255)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from users.serializers import UserSerializer
+from users.models import User
+from django.http import Http404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import APIView
 
-# Create your views here.
+class UserAPIView(APIView):
+    def get(self,request):
+        users= User.objects.all()
+        serializer= UserSerializer(users,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def post(self,request):
+        data= request.data
+        serializer= UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
